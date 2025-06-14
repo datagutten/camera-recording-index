1bfbea0384e9748d523dcf79c5a9af09611c37fb
Anders Birkenes <datagutten@datagutten.net>
2025-06-14 09:43:08 +0200

    Use gevent worker for gunicorn

--- recording_index/gunicorn.conf.py
+++ recording_index/gunicorn.conf.py
@@ -4,3 +4,4 @@ workers = 4
 bind = '0.0.0.0:8000'
 wsgi_app = 'recording_index.wsgi:application'
 secure_scheme_headers = {'X-FORWARDED-PROTOCOL': 'ssl', 'X-FORWARDED-PROTO': 'https', 'X-FORWARDED-SSL': 'on'}
+worker_class = 'gevent'
